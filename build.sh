e11a97439807937d6e24e7387b498d680c1726e7
Kangear <kangear@163.com>
2015-09-14 00:40:37 +0800

    root user run this script

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@
 # This is your user passwd.
 PASSWD=123
 
+if [ `id -u` -ne 0 ]; then
+    echo "Superuser privileges are required to run this script."
+    echo "e.g. \"sudo $0\""
+    exit 1
+fi
+
 # 0. sth
-echo "$PASSWD" | sudo -S apt-get install libusb-dev build-essential libsane-dev libavahi-client-dev libavahi-glib-dev &&
+echo y | apt-get install libusb-dev build-essential libsane-dev libavahi-client-dev libavahi-glib-dev &&
 
 # 1. clean the dir
 make distclean ; 
@@ -16,19 +22,19 @@ make distclean ;
 ./configure --prefix=/usr --libdir=/usr/lib/i386-linux-gnu --sysconfdir=/etc --localstatedir=/var  --enable-avahi BACKENDS="canon_dr test" &&
 
 # 3. build & install
-make -j32 && echo "$PASSWD" | sudo -S make install &&
+make -j32 && make install &&
 
 # 4. cp canon_dr.conf which can not be cp during install.
-echo "$PASSWD" | sudo -S cp backend/canon_dr.conf /etc/sane.d/canon_dr.conf &&
+cp backend/canon_dr.conf /etc/sane.d/canon_dr.conf &&
 
 # 5. ln libsane-canon_dr.so.1.0.25 which can not be link during install.
-echo "$PASSWD" | sudo -S ln -sf /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1.0.25 /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1 &&
+ln -sf /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1.0.25 /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1 &&
 
 echo "build done. running test app..."
 
 # 6. run test
-export SANE_DEBUG_DLL=255 SANE_DEBUG_CANON_DR=255 &&
-echo "$PASSWD" | sudo -S scanimage -L 2>&1 | grep canon
+export SANE_DEBUG_DLL=255 SANE_DEBUG_CANON_DR=255
+scanimage -L 2>&1 | grep canon
 
 # if there is a Scanner has be detected, then you can run this by hand to test.
 # sudo simple-scan
