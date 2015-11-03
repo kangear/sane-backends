a4f59320c05f76f0450b30559a6773ea26916f90
Kangear <kangear@163.com>
2015-11-04 03:17:57 +0800

    build.sh: 添加静态编译scanimage

--- build.sh
+++ build.sh
@@ -32,13 +32,22 @@ PRELOADABLE_BACKENDS="canon_dr" \
 --enable-preload &&
 
 # 3. build & install
-make -j32 && make install &&
+make -j32 &&
+
+# make install &&
+
+# 静态编译scanimage
+cd frontends &&
+gcc -g -O2 -W -Wall -Wcast-align -Wcast-qual -Wmissing-declarations -Wmissing-prototypes -Wpointer-arith -Wreturn-type -Wstrict-prototypes -pedantic -ansi -I/usr/local/include/libusb-1.0 -o scanimage scanimage.o stiff.o  ../backend/.libs/libsane.a -L/usr/local/lib -lm -lieee1284 -ljpeg -lusb-1.0 -lrt -lpthread ../sanei/.libs/libsanei.a ../lib/.libs/liblib.a ../lib/.libs/libfelib.a -pthread -static &&
+cd - &&
 
 # 4. cp canon_dr.conf which can not be cp during install.
-cp backend/canon_dr.conf /etc/sane.d/canon_dr.conf &&
+# cp backend/canon_dr.conf /etc/sane.d/canon_dr.conf &&
 
 # 5. ln libsane-canon_dr.so.1.0.25 which can not be link during install.
-ln -sf /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1.0.25 /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1 &&
+# ln -sf /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1.0.25 /usr/lib/i386-linux-gnu/sane/libsane-canon_dr.so.1 &&
+
+make install DESTDIR=$PWD/_install -j32
 
 echo "build done. running test app..."
 
